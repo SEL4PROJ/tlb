c9d059692e25c56db7aa38576199285b76ee5c29
Hira Taqdees <hira.syeda@data61.csiro.au>
2018-02-03 13:37:45 +1100

    ptable_ops

--- Case_Study/Page_Table_Ops.thy
+++ Case_Study/Page_Table_Ops.thy
@@ -8,7 +8,7 @@ begin
 
 
 lemma remap_section_current_asid:
-  "\<Turnstile> \<lbrace>\<lambda>s. mmu_layout s \<and> mode s = Kernel \<and>
+  "\<Turnstile> \<lbrace>\<lambda>s. mmu_layout s \<and> mode s = Kernel \<and> safe_set (kernel_safe s) s \<and> 
       Addr vp \<in> SM \<and>
       SM = kernel_safe s \<and>
       asids_consistent {} s \<and>
@@ -18,51 +18,68 @@ lemma remap_section_current_asid:
       decode_pde pde' = SectionPDE base' perms' \<rbrace>
       Const vp ::= Const pde'
    \<lbrace>\<lambda>s. asids_consistent {asid s} s\<rbrace>"
-  apply (vcgm vcg:  assign_sound)
+  apply (vcgm vcg: weak_pre_write')
   apply (rule conjI)
-   apply (clarsimp simp: asids_consistent_def mmu_layout_def ran_def)
-   apply blast
-  apply (clarsimp simp: kernel_safe_def mmu_layout_def)
-  apply (clarsimp simp: asids_consistent_def)
-  apply (subgoal_tac "root_map (s\<lparr> heap := heap s(Addr (vp - global_offset) \<mapsto> pde')\<rparr>) = root_map s")
-   apply clarsimp
-   apply (clarsimp simp: ptable_comp_def)
-   apply (clarsimp simp: ran_def)
-   apply blast
-  apply (clarsimp simp: kernel_data_def )
-  apply (clarsimp simp: ptable_footprint_def k_phy_ad_def)
+   apply (clarsimp simp: asids_consistent_def safe_set_def con_set_def)
+  apply (subgoal_tac " ptable_lift' (heap s) (root s) (Addr vp) = Some (Addr vp r-  global_offset)" , clarsimp)
+   prefer 2
+   apply (clarsimp simp: kerenl_region_offset')
   apply (subgoal_tac "Addr (vp - global_offset) \<notin> root_map_area")
    prefer 2
-   apply blast
-  by clarsimp
+   apply (subgoal_tac "root s \<in> set (root_log s)")
+    apply (clarsimp simp: mmu_layout_def kernel_data_def k_phy_ad_def, blast) 
+   apply (clarsimp simp: mmu_layout_def root_log_def)
+   apply (rule_tac x = "addr_val (root s)" in image_eqI, simp, force)
+  apply (clarsimp simp:asids_consistent_def)
+  apply (subgoal_tac "root_map (s\<lparr> heap := heap s(Addr (vp - global_offset) \<mapsto> pde')\<rparr>) = root_map s")
+   apply clarsimp
+   apply (drule_tac x = r in spec, drule_tac x = a in spec, simp)
+   apply (drule_tac x = v in spec)
+   apply (erule disjE, simp)
+   apply (subgoal_tac "pt_walk a (heap s(Addr (vp - global_offset) \<mapsto> pde')) r v =
+                          pt_walk a (heap s) r v", simp)
+   apply (rule pt_walk_pt_trace_upd')
+   apply simp
+   prefer 2
+   apply (clarsimp simp: root_map_area_def)
+  apply (subgoal_tac "r \<noteq> root s")
+   prefer 2
+   apply (clarsimp simp: mmu_layout_def)
+  apply (clarsimp simp: k_phy_ad_def ptable_footprint_def)
+  apply (subgoal_tac "non_overlapping_tables s")
+   apply (clarsimp simp: non_overlapping_tables_def)
+   apply (drule_tac x = r in spec, drule_tac x = "root s" in spec)
+   apply (subgoal_tac "r \<in> roots s \<and> root s \<in> roots s")
+    apply (clarsimp simp: ptable_footprint_def) apply blast
+   apply rule
+    prefer 2
+    apply (clarsimp simp: roots_def mmu_layout_def root_log_def)
+    apply (rule_tac x = "addr_val (root s)" in image_eqI, simp, force)
+   apply (clarsimp simp: roots_def mmu_layout_def root_log_def)
+   apply (rule_tac x = "addr_val r" in image_eqI, simp, force)
+  sorry
  
-  
+
+lemma
+  "mmu_layout s \<Longrightarrow> non_overlapping_tables s"
+  apply (clarsimp simp: mmu_layout_def kernel_data_def)
+  apply (clarsimp simp: non_overlapping_tables_def)
+  apply (clarsimp simp: roots_def)
+  apply (clarsimp simp: ptable_footprint_def root_log_def)
+
+  sorry
 
 
-
-
-lemma remap_section_current_strong:
-  "\<Turnstile> \<lbrace>\<lambda>s. mmu_layout s \<and> mode s = Kernel \<and>
-      Addr vp \<in> SM \<and>
-      SM = kernel_safe s \<and>
-      asids_consistent {} s \<and>
-      k_phy_ad vp \<in> ptable_footprint s (root s) \<and>
-      heap s (k_phy_ad vp) = Some pde \<and>
-      (\<exists>p pbits. decode_pde pde = SectionPDE p pbits) \<and> 
-      decode_pde pde' = SectionPDE base permissions \<and>
-      {base .. base r+ section_size} \<inter> kernel_phy_mem = {} \<rbrace>
-      Const vp ::= Const pde'
-   \<lbrace>\<lambda>s. mmu_layout s \<and> asids_consistent {asid s} s \<and> mode s = Kernel \<and> safe_set (kernel_safe s) s\<rbrace>"
-   oops (* would be nicer, but we probably don't have enough time, probably needs stronger preconds *)
 
 lemma kernel_safe_incon_upd[simp]:
   "kernel_safe (incon_set_update f s) = kernel_safe s"
-  by (simp add: kernel_safe_def vas_of_current_state_mapped_to_global_mappings_of_all_processes_def)
+  by (simp add: kernel_safe_def vas_of_current_state_mapped_to_global_mappings_of_all_processes_def
+    roots_def)
 
 
 lemma kernel_safe_ptable_snapshot_upd[simp]:
   "kernel_safe (ptable_snapshot_update f s) = kernel_safe s"
-  by (simp add: kernel_safe_def vas_of_current_state_mapped_to_global_mappings_of_all_processes_def)
+  by (simp add: kernel_safe_def vas_of_current_state_mapped_to_global_mappings_of_all_processes_def roots_def)
 
 
 lemma flush_one_asid:
@@ -70,9 +87,8 @@ lemma flush_one_asid:
        Flush (flushASID a)
    \<lbrace>\<lambda>s. asids_consistent {} s\<rbrace>"
    apply vcgm
-   apply (simp add: asids_consistent_def Times_Diff_distrib1 Diff_Int_distrib2)
-   apply fastforce
-   done
+   by (simp add: asids_consistent_def Times_Diff_distrib1 Diff_Int_distrib2)
+
 
 
 lemma flush_one_asid_rest:
@@ -80,9 +96,8 @@ lemma flush_one_asid_rest:
        Flush (flushASID a)
    \<lbrace>\<lambda>s. mmu_layout s \<and> mode s = Kernel \<and> safe_set (kernel_safe s) s\<rbrace>"
   apply vcgm
-  by (simp add: mmu_layout_def safe_set_def safe_memory_def con_set_def ptrace_set_def)
-
-
+  by (simp add: mmu_layout_def safe_set_def safe_memory_def con_set_def ptrace_set_def kernel_data_def)
+  
 
 
 end
